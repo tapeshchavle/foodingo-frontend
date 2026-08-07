97e0ca19dc4f4e47ec35df01472a2b52e6e983c0
tapeshchavle <tapeshchawle@gmail.com>
2026-08-07 18:31:27 +0530

    added global variable

--- src/pages/MyOrders/myorder.js
+++ src/pages/MyOrders/myorder.js
@@ -1,27 +1,24 @@
-import axios from "axios";
-const APP_URL = "https://foodingo-api-awasc8h4d7d7cmft.centralindia-01.azurewebsites.net";
+import { api } from "../../config/api.js";
+
 export const getAllOrders = async (token) => {
   try {
-    const response = await axios.get(`${APP_URL}/api/orders`, { headers: { Authorization: `Bearer ${token}` } })
+    const response = await api.get("/api/orders", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response;
   } catch (error) {
     console.log("some error ");
   }
-}
+};
 
 export const getOrderByOrderId = async (orderId, token) => {
   try {
-    const response = await axios.get(
-      `${APP_URL}/api/orders/${orderId}`,
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
-    //console.log("order is:", response);
+    const response = await api.get(`/api/orders/${orderId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response;
   } catch (error) {
     console.log("some error", error);
-    throw error; // optional: re-throw for caller to handle
+    throw error;
   }
 };
-
